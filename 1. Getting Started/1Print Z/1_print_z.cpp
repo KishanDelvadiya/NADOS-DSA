17c48fffdf4e85e30cc32181c7d8c09815aa7a74
KishanDelvadiya <82096703+KishanDelvadiya@users.noreply.github.com>
2022-04-24 13:59:57 +0530

    Create 1_print_z.cpp
    Print Z
    Easy
    
    1. You are required to print a 'z' of size 5 using '*'.
    
    Constraints
    No Constraints
    
    Format
    Input
    There is no input
    
    
    Example
    Sample Input
    
    No Input
    
    Sample Output
    *****
       *
      *
     *
    *****

---
+++ 1. Getting Started/1Print Z/1_print_z.cpp
@@ -0,0 +1,23 @@
+/*
+
+*****
+   *
+  *
+ *
+*****
+
+
+
+*/
+
+#include<iostream>
+using namespace std;
+int main(int argc, char** agrc){
+    //write your code here
+    printf("*****\n");
+    printf("   *\n");
+    printf("  *\n");
+    printf(" *\n");
+    printf("*****\n");
+
+}
