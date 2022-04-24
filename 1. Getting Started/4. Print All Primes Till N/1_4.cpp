ab2ae612fea8b566825b796675a69867a9a9282f
KishanDelvadiya <82096703+KishanDelvadiya@users.noreply.github.com>
2022-04-24 14:16:06 +0530

    Create 1_4.cpp

---
+++ 1. Getting Started/4. Print All Primes Till N/1_4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+
+bool isPrime(int );
+
+int main(int argc, char **argv){
+    int low, high;
+    cin >> low >> high;
+
+    for(int i=low; i <= high; ++i)
+    {
+        if(isPrime(i))
+            cout << i << "\n";
+    }
+
+}
+
+
+bool isPrime(int n)
+{
+    for (int i = 2; i*i <= n; ++i)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
